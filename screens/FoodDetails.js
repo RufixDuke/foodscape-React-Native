1d06731b9ab572b30593e5c7c96879f66eb51d84
RufixDuke <rufixduke01@gmail.com>
2022-07-16 18:39:48 +0100

    Food details screen

--- screens/FoodDetails.js
+++ screens/FoodDetails.js
@@ -13,11 +13,8 @@ const FoodDetails = ({ navigation }) => {
             .then((response) => response.json())
             .then((recipe) => {
                 setDetails(recipe.recipe);
-                // console.log(recipe);
             });
     };
-
-    // console.log(details.pu);
 
     useEffect(() => {
         fetchDetails();
@@ -45,42 +42,73 @@ const FoodDetails = ({ navigation }) => {
                     <Image
                         source={require("../assets/icons/location-filled.png")}
                     />
-                    <Text
-                        style={{
-                            color: "#333333",
-                            fontWeight: "600",
-                            fontSize: 14,
-                        }}
-                    >
-                        {details.publisher}
-                    </Text>
+                    <Text style={styles.detailsText}>{details.publisher}</Text>
                 </View>
 
                 <View style={{ flexDirection: "row", alignItems: "center" }}>
                     <Image
                         source={require("../assets/icons/clock-filled.png")}
                     />
-                    <Text
-                        style={{
-                            color: "#333333",
-                            fontWeight: "600",
-                            fontSize: 14,
-                        }}
-                    >
+                    <Text style={styles.detailsText}>
                         {(details.social_rank - 10) / 3} minutes
                     </Text>
                 </View>
 
                 <View style={{ flexDirection: "row", alignItems: "center" }}>
                     <Image source={require("../assets/icons/distance.png")} />
-                    <Text
-                        style={{
-                            color: "#333333",
-                            fontWeight: "600",
-                            fontSize: 14,
-                        }}
-                    >
-                        {(details.social_rank - 10) / 3} minutes
+                    <Text style={styles.detailsText}>
+                        {(details.social_rank + 10) / 100} Km
+                    </Text>
+                </View>
+            </View>
+
+            <View style={styles.ingredients}>
+                <Text
+                    style={{
+                        color: "#333333",
+                        fontSize: 18,
+                        fontWeight: "600",
+                        textAlign: "center",
+                        marginTop: 10,
+                        textDecorationLine: "underline",
+                    }}
+                >
+                    Ingredients
+                </Text>
+                <Text style={styles.ingreDetails}>
+                    {details.ingredients.join()}
+                </Text>
+            </View>
+
+            <View
+                style={{
+                    marginTop: 40,
+                    marginHorizontal: 20,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                }}
+            >
+                <View>
+                    <Text style={{ color: "#4F4F4F", fontWeight: "600" }}>
+                        {(details.recipe_id / 200).toFixed()} g |{" "}
+                        {(details.recipe_id / 270).toFixed()} cal
+                    </Text>
+                </View>
+
+                <View
+                    style={{
+                        flexDirection: "row",
+                        alignItems: "center",
+                    }}
+                >
+                    <Image
+                        source={require("../assets/icons/star-filled.png")}
+                        style={{ width: 11, height: 11, marginRight: 1 }}
+                    />
+                    <Text style={{ color: "#4F4F4F", fontWeight: "600" }}>
+                        {(details.social_rank / 21).toFixed(1)}(
+                        {(details.social_rank + 47).toFixed()})
                     </Text>
                 </View>
             </View>
@@ -120,6 +148,21 @@ const styles = StyleSheet.create({
         justifyContent: "space-between",
         alignItems: "center",
         marginHorizontal: 20,
+    },
+    detailsText: {
+        color: "#333333",
+        fontWeight: "600",
+        fontSize: 14,
+    },
+    ingredients: {
+        marginHorizontal: 20,
+    },
+    ingreDetails: {
+        color: "#4F4F4F",
+        lineHeight: 20,
+        fontSize: 16,
+        textAlign: "center",
+        marginTop: 10,
     },
 });
 
